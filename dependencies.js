4506f6bfe728102443e5b55f0be20293ad095a6d
Ansar <md.atif.ansar@accenture.com>
2024-03-24 16:35:34 +0530

    minor bux fix

--- dependencies.js
+++ dependencies.js
@@ -34,9 +34,9 @@ module.exports = (app, express) => {
   );
 
   if (process.env.NODE_ENV == "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("front/build"));
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "front", "build", "index.html"));
     });
   }
 };
