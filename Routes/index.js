b1813a1efb90f0ab22eba2bf1f21e46da88c7590
Ansar <md.atif.ansar@accenture.com>
2024-03-31 12:27:52 +0530

    deploy ready!

--- Routes/index.js
+++ Routes/index.js
@@ -1,12 +1,13 @@
 const authRoute = require("../api/authentication/index");
 const assRoute = require("../api/assignment/index");
 const classRoute = require("../api/classroom/index");
+const { createError } = require("../utils/utilFunctions");
 
 module.exports = (app) => {
-  app.get("/", (req, res) => {
-    res.send("<h3>welcome to tutors point</h3>");
-  });
   app.use("/auth", authRoute);
   app.use("/asn", assRoute);
   app.use("/class", classRoute);
+  app.get("/*", (req, res, next) => {
+    return next(createError("Not Found!", 404));
+  });
 };
